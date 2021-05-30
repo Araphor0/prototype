2c44d0774f537079f920ec75a6624a2a4b0454ec
Matthew Squire <Matthewjsquire@outlook.com>
2021-05-30 17:34:14 +0100

    Simple commenting

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,5 @@
 /*
-This is the prototype of the trading bot in Rust.
-
-The aim is to fix the bug and provide a suitable
-codebase to access the coinbase API.
-
-base64 = "0.2"
-hyper = "0.9"
-rust-crypto = "0.2"
-serde = "*"
-serde_json = "*"
-serde_macros = "*"
-time = "0.1"
-
-[dependencies.chrono]
-version = "0.2.25"
-features = ["serde"]
-
-[dependencies.uuid]
-version = "0.3.1"
-features = ["serde"]
-
-[dev-dependencies]
-env_logger = "0.3"
-uuid = { version = "0.3.1", features = ["v4"] }
+This is the prototype of the Rust bot project
 */
 
 fn main() {
