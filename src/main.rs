f578d24cd8b378af32ed0457eccddd28f1ba162c
Matthew Squire <Matthewjsquire@outlook.com>
2021-06-07 18:23:14 +0100

    Entire new build, got rid of the old crate and now using a updated and better crate for trading, therefore I do not need a integrated library that needs maintaining

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use coinbase_pro_rs::{Public, Sync, SANDBOX_URL};
 /*
 This is the prototype of the Rust bot project
 */
 
 fn main() {
     println!("Keep building!");
+    let client: Public<Sync> = Public::new(SANDBOX_URL);
+    let time = client.get_time().unwrap();
+    println!("Coinbase.time: {}", time.iso);
 }
