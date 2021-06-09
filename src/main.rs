cf08f4fb974852e694b3a199f0a2518d7f43c4f8
Matthew Squire <Matthewjsquire@outlook.com>
2021-06-09 15:49:26 +0100

    Example of coding orders

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,19 @@ use coinbase_pro_rs::{Private, Sync, SANDBOX_URL};
 mod keys;
 use keys::{key, passphrase, secret};
 fn main() {
-    let _client: Private<Sync> = Private::new(SANDBOX_URL, key(), secret(), passphrase());
+    let client: Private<Sync> = Private::new(SANDBOX_URL, key(), secret(), passphrase());
     println!("Keep building!");
+
+    /* Example code of what a buy and sell market order would consist of in code. */
+    //client.buy_market_funds("ETH-GBP", 10.0).expect("Could not buy market funds");
+    //client.sell_market_funds("ETH-GBP", 10.11).expect("Could not sell market");
+
+    loop {
+        client
+            .buy_market_funds("ETH-GBP", 10.0)
+            .expect("could not buy market funds");
+        client
+            .sell_market_funds("ETH-GBP", 10.11)
+            .expect("Could not sell market funds");
+    }
 }
