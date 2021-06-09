760ce8f7e90ef092eaeddb65829555f6af7ba61c
Matthew Squire <Matthewjsquire@outlook.com>
2021-06-09 16:02:07 +0100

    Removing fake credentials

--- src/keys/mod.rs
+++ src/keys/mod.rs
@@ -4,17 +4,17 @@ by doing this, the main file is looking less like unorganised code,
 */
 
 pub fn key() -> &'static str {
-    static KEY: &str = "1d0dc0f7b4e808d430b95d8fed7df3ea";
+    static KEY: &str = "";
     KEY
 }
 
 pub fn secret() -> &'static str {
     static SECRET: &str =
-        "dTUic8DZPqkS77vxhJFEX5IBr13FcFHTzWYOARgT9kDWGdN03uvxBbH/hVy8f4O5RDmuf+9wNpEfhYhw2FCWyA==";
+        "";
     SECRET
 }
 
 pub fn passphrase() -> &'static str {
-    static PASSPHRASE: &str = "sandbox";
+    static PASSPHRASE: &str = "";
     PASSPHRASE
 }
