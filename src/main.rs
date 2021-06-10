96be27577c1aafbda4dba9197b0f01eafb69a726
Matthew Squire <Matthewjsquire@outlook.com>
2021-06-10 10:52:15 +0100

    Implmenting number_of_trades as 3600

--- src/main.rs
+++ src/main.rs
@@ -29,19 +29,32 @@ fn main() {
     println!("{}.available: {:?}", usd.currency, usd.available);
     println!("{}.     hold: {:?}", usd.currency, usd.hold);
 
+    let mut number_of_trades: i32 = 1;
     loop {
-        client
-            .buy_market_funds("BTC-USD", 10.00)
-            .expect("Could not buy_market_funds");
-        client
-            .sell_market_funds("BTC-USD", 10.00)
-            .expect("Could not sell_market_funds");
-        
-        //Stop Loss function.
-        if usd.balance <= 5.00 {
-            println!("[Breaking loop]");
+        if number_of_trades == 3600 {
+            println!("[Finishing]");
             break;
+        } else {
+            client
+                .buy_market_funds("BTC-USD", 10.00)
+                .expect("Could not buy_market_funds");
+            client
+                .sell_market_funds("BTC-USD", 10.00)
+                .expect("Could not sell_market_funds");
+
+            number_of_trades += 1;
+
+            //Stop Loss function.
+            if usd.balance <= 5.00 {
+                println!("[Breaking loop]");
+                break;
+            }
         }
         thread::sleep(Duration::from_millis(200));
     }
+    println!("United States Dollar:");
+    let usd = accounts.iter().find(|x| x.currency == "USD").unwrap();
+    println!("{}.  balance: {:?}", usd.currency, usd.balance);
+    println!("{}.available: {:?}", usd.currency, usd.available);
+    println!("{}.     hold: {:?}", usd.currency, usd.hold);
 }
